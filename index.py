d8e20e610d00c1cd46517a3926284abe37952b36
keza-laura <k.laura@alustudent.com>
2025-10-17 19:54:29 +0200

    initial commit

---
+++ index.py
@@ -0,0 +1,14 @@
+print("Hello Everyone!")
+
+
+number=42
+number=100
+nnumber=42
+
+#id acts as a pointer to the memory location of the variable (for data stored)
+print(id(42))
+print(id(number))
+print(id(nnumber))
+
+
+
