af9ae56f8a11b6b1bcfdddb29e67678fed80bc14
keza-laura <k.laura@alustudent.com>
2025-10-27 11:07:58 -0700

    data

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    """Return a tuple with the length of the sentence and its first character."""
+    first_char = sentence[0] if sentence else None
+    return (len(sentence), first_char)
