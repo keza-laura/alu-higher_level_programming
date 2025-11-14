e4a6f8e52a9cc294cda230145966929c97bf7b4a
keza-laura <k.laura@alustudent.com>
2025-11-14 12:14:27 -0800

    I/O

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,9 +3,10 @@
 Module defining a Student class
 """
 
+
 class Student:
     """Class representing a student"""
-    
+
     def __init__(self, first_name, last_name, age):
         """Instantiation with first_name, last_name, and age"""
         self.first_name = first_name
