77e84d6de30cc24af5d7b95cccd6e9331e855263
keza-laura <k.laura@alustudent.com>
2025-11-14 11:03:48 -0800

    I/O

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ This module defines a function to append a string to a UTF-8 text file.
 
 def append_write(filename="", text=""):
     """
-    Appends a string to a UTF-8 text file. Creates the file if it does not exist.
+    Appends a string to a UTF-8 text file.
 
     Args:
         filename (str): The path to the file
