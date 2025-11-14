213e41cfa622d754b046bbb375c778c09aadf9a6
keza-laura <k.laura@alustudent.com>
2025-11-14 12:11:26 -0800

    I/O

---
+++ python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module defining a Student class
+"""
+
+class Student:
+    """Class representing a student"""
+    
+    def __init__(self, first_name, last_name, age):
+        """Instantiation with first_name, last_name, and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns the dictionary representation of a Student instance"""
+        return self.__dict__
