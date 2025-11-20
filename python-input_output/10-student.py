d5a476e638fb9d616a13fea045583055e7972c9c
keza-laura <k.laura@alustudent.com>
2025-11-20 11:44:01 -0800

    10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -7,9 +7,10 @@ Module defining a Student class with flexible JSON serialization
 class Student:
     """Class representing a student"""
 
-
     def __init__(self, first_name, last_name, age):
+
         """Instantiation with first_name, last_name, and age"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
