156beefa3711fa3352bd31bf732a06f6bedd26c5
keza-laura <k.laura@alustudent.com>
2026-01-29 10:00:12 -0800

    js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (Number.isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = Number.parseInt(process.argv[2]);
+
+console.log(factorial(num));
