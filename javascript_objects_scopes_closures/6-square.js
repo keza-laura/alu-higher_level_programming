e0e097b184dfedb751de43fcfa72f111ab02813a
keza-laura <k.laura@alustudent.com>
2026-01-29 11:53:18 -0800

    js

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@
 const SquareBase = require('./5-square');
 
 class Square extends SquareBase {
-  charPrint(c) {
+  charPrint (c) {
     const char = c || 'X';
     for (let i = 0; i < this.height; i++) {
       console.log(char.repeat(this.width));
