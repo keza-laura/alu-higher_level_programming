720e66e3bd42e7b6d8da997e29f8a96611960fe5
keza-laura <k.laura@alustudent.com>
2026-01-29 09:49:07 -0800

    js

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const times = Number.parseInt(process.argv[2]);
+
+if (Number.isNaN(times)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < times; i++) {
+    console.log('C is fun');
+  }
+}
